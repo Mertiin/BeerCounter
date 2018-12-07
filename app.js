7627be9f6ee2b45995a23d2331175a70a96ad2b6
Martin Lundberg <martin.lundberg@netigate.se>
2018-12-07 15:46:00 +0100

    Init

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+
+const data = {}
+
+app.get('/', (req, res) => {
+  res.send(data)
+})
+
+app.post('/', (req, res) => {
+  if (!data[req.body.type]) data[req.body.type] = 0
+  data[req.body.type] += req.body.count || 1
+  res.send(data)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
